857fa5ad90a8763562d700606c9cc3f6e1758b74
Scott Reilly <scott@coffee2code.com>
2008-01-18 21:11:15 +0000

    Release v0.9.5:
    * Minor code and formatting tweaks
    * Update description
    * Update copyright

--- single-category-permalink.php
+++ single-category-permalink.php
@@ -1,14 +1,16 @@
 <?php
 /*
 Plugin Name: Single Category Permalink
-Version: 0.9.2
+Version: 0.9.5
 Plugin URI: http://www.coffee2code.com/wp-plugins/
 Author: Scott Reilly
 Author URI: http://www.coffee2code.com
-Description: Modifies the %category% permalink structure tag to generate a category-based permalink structure that only displays the lowest category in a hierarchy, as opposed to the entire hierarchy of categories.
+Description: Modify the %category% permalink structure tag to generate a category-based permalink structure that only displays the lowest category in a hierarchy, as opposed to the entire hierarchy of categories.
 
+Compatible with WordPress 1.5+, 2.0+, 2.1+, 2.2+, and 2.3+.
 
-=>> Visit the plugin's homepage for more information and latest updates  <<=
+=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
+=>> for more information and the latest updates
 
 Installation:
 
@@ -17,14 +19,13 @@ Installation:
 -OR-
 Copy and paste the the code ( http://www.coffee2code.com/wp-plugins/single-category-permalink.phps ) into a file called 
 single-category-permalink.php, and put that file into your /wp-content/plugins/ directory.
-2. Activate the plugin from your WordPress admin 'Plugins' page.
-
-See the plugin's homepage for usage instructions.
+2. Activate the plugin through the 'Plugins' admin menu in WordPress
+3. Use %category% as a permalink tag in the 'Options' -> 'Permalinks' admin options page when defining a custom permalink structure
 
 */
 
 /*
-Copyright (c) 2007 by Scott Reilly (aka coffee2code)
+Copyright (c) 2007-2008 by Scott Reilly (aka coffee2code)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
 files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
@@ -42,55 +43,39 @@ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 function single_category_postlink($permalink, $post) {
 	$permalink_structure = get_option('permalink_structure');
     if (strpos($permalink_structure, '%category%') !== false) {
-            $cats = get_the_category($post->ID);
-            if ( $cats )
-                    usort($cats, '_usort_terms_by_ID'); // order by ID
-            $category = $cats[0]->slug;
-            if ( $parent=$cats[0]->parent )
-                    $category = get_category_parents($parent, FALSE, '/', TRUE) . $category;
-			// Now we know what the permalink component involving category hierarchy consists of.  Get rid of it.
-			$permalink = str_replace($category, $cats[0]->slug, $permalink);
+		$cats = get_the_category($post->ID);
+		if ( $cats )
+			usort($cats, '_usort_terms_by_ID'); // order by ID
+		$category = $cats[0]->slug;
+		if ( $parent=$cats[0]->parent )
+			$category = get_category_parents($parent, FALSE, '/', TRUE) . $category;
+		// Now we know what the permalink component involving category hierarchy consists of.  Get rid of it.
+		$permalink = str_replace($category, $cats[0]->slug, $permalink);
     }
 	return $permalink;
 }
-add_filter('post_link', 'single_category_postlink', 10, 2);
 
 function single_category_catlink($catlink, $category_id) {
 	global $wp_rewrite;
 	$catlink = $wp_rewrite->get_category_permastruct();
 
 	if ( empty($catlink) ) {
-	        $file = get_option('home') . '/';
-	        $catlink = $file . '?cat=' . $category_id;
+		$file = get_option('home') . '/';
+		$catlink = $file . '?cat=' . $category_id;
 	} else {
-	        $category = &get_category($category_id);
-	        if ( is_wp_error( $category ) )
-	                return $category;
-	        $category_nicename = $category->slug;
-
-			$catlink = str_replace('/category/', '/', $catlink);
-	        $catlink = str_replace('%category%', $category_nicename, $catlink);
-	        $catlink = get_option('home') . user_trailingslashit($catlink, 'category');
-	}
-	return $catlink;
-
-
-	if (strpos($cattag, $catlink) !== false) {
 		$category = &get_category($category_id);
 		if ( is_wp_error( $category ) )
-		        return $category;
+			return $category;
 		$category_nicename = $category->slug;
+
+		$catlink = str_replace('/category/', '/', $catlink);
 		$catlink = str_replace('%category%', $category_nicename, $catlink);
 		$catlink = get_option('home') . user_trailingslashit($catlink, 'category');
 	}
 	return $catlink;
 }
-add_filter('category_link', 'single_category_catlink', 10, 2);
 
-//function add_singlecategory_rewrite() {
-//	global $single_category_permalink_tag;
-//	add_rewrite_tag($single_category_permalink_tag, '(.+)');
-//}
-//add_action('init', 'add_singlecategory_rewrite');
+add_filter('post_link', 'single_category_postlink', 10, 2);
+add_filter('category_link', 'single_category_catlink', 10, 2);
 
 ?>
