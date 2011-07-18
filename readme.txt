5b8f5f44f13aa7feba5148a0a50383591f5f0aee
Scott Reilly <scott@coffee2code.com>
2011-07-18 02:36:47 -0400

    Release v2.0:
    * Fix compatibility bug relating to generation of category permalink
    * Rename `single_category_postlink()` to `c2c_single_category_postlink()`
    * Rename `single_category_catlink()` to `c2c_single_category_catlink()`
    * Add `c2c_single_category_redirect()` to redirect hierarchical category links to the single category alternative
    * Add filter 'c2c_single_category_redirect_status' to allow override of default redirect status code
    * Wrap all functions in `if (!function_exists())` check
    * Remove docs from top of plugin file (all that and more are in readme.txt)
    * Add plugin homepage and author links in description in readme.txt
    * Note compatibility through WP3.2+
    * Add PHPDoc documentation
    * Expand documentation in readme.txt
    * Minor tweaks to code formatting (spacing)
    * Minor documentation reformatting in readme.txt
    * Change description
    * Add package info to top of plugin file
    * Add Frequently Asked Questions, Filters, Changelog, and Upgrade Notice sections to readme.txt
    * Update copyright date (2011)

--- readme.txt
+++ readme.txt
@@ -1,38 +1,122 @@
 === Single Category Permalink ===
 Contributors: coffee2code
-Donate link: http://coffee2code.com
-Tags: permalink, structure, link, category
+Donate link: http://coffee2code.com/donate
+Tags: permalink, structure, link, category, coffee2code
 Requires at least: 1.5
-Tested up to: 2.5
-Stable tag: trunk
-Version: 1.0
+Tested up to: 3.2
+Stable tag: 2.0
+Version: 2.0
 
-Modify the `%category%` permalink structure tag to generate a category-based permalink structure that only displays the lowest category in a hierarchy, as opposed to the entire hierarchy of categories.
+Reduce permalinks (category or post) that include entire hierarchy of categories to just having the lowest level category.
+
 
 == Description ==
 
-Modify the `%category%` permalink structure tag to generate a category-based permalink structure that only displays the lowest category in a hierarchy, as opposed to the entire hierarchy of categories.
+Reduce permalinks (category or post) that include entire hierarchy of categories to just having the lowest category in the hierarchy.
 
 By default, WordPress replaces the %category% permalink tag in a custom permalink structure with the entire hierarchy of categories for the post's first matching category.  For example, assuming your site has a hierarchical category structure like so:
 
+`
 Applications
   |_ Desktop
   |_ Web
-    |_ Wordpress
+    |_ WordPress
+`
 
-By default, if you have a permalink structure defined as `%category%/%year%/%monthnum%/%day%/%postname%`, your post titled "Best Plugins" assigned to the 
-"WordPress" category would have a permalink of:
-http://www.yourblog.com/applications/web/wordpress/2008/01/15/best-plugins
+By default, if you have a permalink structure defined as `%category%/%year%/%monthnum%/%day%/%postname%`, your post titled "Best Plugins" assigned to the "WordPress" category would have a permalink of:
+
+`http://www.yourblog.com/applications/web/wordpress/2008/01/15/best-plugins`
 
 If you activate the Single Category Permalink plugin, this would be the permalink generated for the post (and recognized by the blog):
-http://www.yourblog.com/wordpress/2008/01/15/best-plugins
 
-In order for a category to be used as part of a post's permalink structure, %category% must be explicitly defined in the Options -> Permalinks (or in WP 2.5: Settings -> Permalinks) admin page as part of a custom structure, i.e. `/%category%/%postname%`.
+`http://www.yourblog.com/wordpress/2008/01/15/best-plugins`
 
-For category links, `%category%` is implied to follow the value set as the "Category base" (or the default category base if none is specified).
+In order for a category to be used as part of a post's permalink structure, %category% must be explicitly defined in the Settings -> Permalinks admin page as part of a custom structure, i.e. `/%category%/%postname%`.
+
+For category links, `%category%` is implied to follow the value set as the "Category base" (or the default category base if none is specified).  so if your category base is 'category', the above example would list posts in the 'WordPress' on this category listing page:
+
+`http://www.yourblog.com/category/applications/web/wordpress/`
+
+With this plugin activated, that link would become:
+
+`http://www.yourblog.com/category/wordpress/`
+
+NOTE: The fully hierarchical category and post permalinks will continue to work.  The plugin issues are 302 redirect to browsers and search engines pointing them to the shorter URL.
+
+Links: [Plugin Homepage](http://coffee2code.com/wp-plugins/single-category-permalink/) | [Author Homepage](http://coffee2code.com)
+
 
 == Installation ==
 
-1. Unzip `single-category-permalink.zip` inside the `/wp-content/plugins/` directory, or upload `single-category-permalink.php` into `/wp-content/plugins/`
+1. Unzip `single-category-permalink.zip` inside the `/wp-content/plugins/` directory for your site (or install via the built-in WordPress plugin installer)
 1. Activate the plugin through the 'Plugins' admin menu in WordPress
-1. Use `%category%` as a permalink tag in the `Options` -> `Permalinks` admin options page when defining a custom permalink structure
+1. Use `%category%` as a permalink tag in the `Settings` -> `Permalinks` admin options page when defining a custom permalink structure
+
+
+== Frequently Asked Questions ==
+
+= Will existing links to my site that used the full category hierarchy still work? =
+
+Yes, WordPress will still serve the category listings and posts regardless of whether it is of the full category hierarchy format or just the single category format.  But do note that WordPress doesn't perform any sort of redirects; it responds directly to the category/post URL requested.
+
+= Could this give the appearance that I have duplicate content on my site if pages are accessible via the full category hierarchy permalink format and the single category permalink format? =
+
+Whether this plugin is active or not, WordPress treats both types of category links the same.  This plugin will however issue redirects for all of the non-canonical category and post permalink pages to point to the single category link version.
+
+= What can this plugin do for me if I don't use `%category%` in my custom permalink structure? =
+
+In addition to handling custom permalink structures (used to generate permalinks for posts) that contain `%category%`, the plugin also shortens category archive links.  WordPress by default generates those links in a fully hierarchical fashion which this plugin will reduce to a single category.  See the Description section for an example.
+
+
+== Filters ==
+
+The plugin exposes one filter for hooking.  Typically, customizations utilizing this hook would be put into your active theme's functions.php file, or used by another plugin.
+
+= c2c_single_category_redirect_status (filter) =
+
+The 'c2c_single_category_redirect_status' hook allows you to specify an HTTP status code used for the redirect.  By default this is 302.
+
+Arguments:
+
+* $status (integer) : The default HTTP status code
+
+Example:
+
+`
+// Change single category redirect to 301
+function scp_change_redirect_status( $code ) {
+	return 301;
+}
+add_filter( 'c2c_single_category_redirect_status', 'scp_change_redirect_status' );
+`
+
+
+== Changelog ==
+
+= 2.0 =
+* Fix compatibility bug relating to generation of category permalink
+* Rename single_category_postlink() to c2c_single_category_postlink()
+* Rename single_category_catlink() to c2c_single_category_catlink()
+* Add c2c_single_category_redirect() to redirect hierarchical category links to the single category alternative
+* Add filter 'c2c_single_category_redirect_status' to allow override of default redirect status code
+* Wrap all functions in if (!function_exists()) check
+* Remove docs from top of plugin file (all that and more are in readme.txt)
+* Add plugin homepage and author links in description in readme.txt
+* Note compatibility through WP3.2+
+* Add PHPDoc documentation
+* Expand documentation in readme.txt
+* Minor tweaks to code formatting (spacing)
+* Minor documentation reformatting in readme.txt
+* Change description
+* Add package info to top of plugin file
+* Add Frequently Asked Questions, Filters, Changelog, and Upgrade Notice sections to readme.txt
+* Update copyright date (2011)
+
+= 1.0 =
+* Initial release
+
+
+== Upgrade Notice ==
+
+= 2.0 =
+Recommended update. No functional changes, but many changes to formatting and documentation; noted compatibility through WP 3.2.
--- single-category-permalink.php
+++ single-category-permalink.php
@@ -1,55 +1,27 @@
 <?php
+/**
+ * @package Single_Category_Permalink
+ * @author Scott Reilly
+ * @version 2.0
+ */
 /*
 Plugin Name: Single Category Permalink
-Version: 1.0
+Version: 2.0
 Plugin URI: http://coffee2code.com/wp-plugins/single-category-permalink
 Author: Scott Reilly
 Author URI: http://coffee2code.com
-Description: Modify the %category% permalink structure tag to generate a category-based permalink structure that only displays the lowest category in a hierarchy, as opposed to the entire hierarchy of categories.
+Description: Reduce permalinks (category or post) that include entire hierarchy of categories to just having the lowest level category.
 
-By default, WordPress replaces the %category% permalink tag in a custom permalink structure with the entire 
-hierarchy of categories for the post's first matching category.  For example, assuming your site has a 
-hierarchical category structure like so:
+Compatible with WordPress 1.5+, 2.0+, 2.1+, 2.2+, 2.3+, 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+, 3.1+, 3.2+.
 
-	Applications
-	  |_ Desktop
-	  |_ Web
-	    |_ Wordpress
-
-By default, if you have a permalink structure defined as "%category%/%year%/%monthnum%/%day%/%postname%",
-your post titled "Best Plugins" assigned to the "WordPress" category would have a permalink of:
-
-http://www.yourblog.com/applications/web/wordpress/2008/01/15/best-plugins
-
-If you activate the Single Category Permalink plugin, this would be the permalink generated for the post
-(and recognized by the blog):
-
-http://www.yourblog.com/wordpress/2008/01/15/best-plugins
-	
-In order for a category to be used as part of a post's permalink structure, %category% must be explicitly defined in
-the Options -> Permalinks (or in WP 2.5: Settings -> Permalinks) admin page as part of a custom structure, i.e.
-"/%category%/%postname%".
-
-For category links, %category% is implied to follow the value set as the "Category base" (or the default category base
-if none is specified).
-
-Compatible with WordPress 1.5+, 2.0+, 2.1+, 2.2+, 2.3+, and 2.5.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/single-category-permalink.zip and unzip it into your 
-/wp-content/plugins/ directory.
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Use %category% as a permalink tag in the 'Options' -> 'Permalinks' (or in WP 2.5: 'Settings' -> 'Permalinks')
-admin options page when defining a custom permalink structure
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/single-category-permalinks/
 
 */
 
 /*
-Copyright (c) 2007-2008 by Scott Reilly (aka coffee2code)
+Copyright (c) 2007-2011 by Scott Reilly (aka coffee2code)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
 files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
@@ -64,42 +36,100 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-function single_category_postlink($permalink, $post) {
-	$permalink_structure = get_option('permalink_structure');
-    if (strpos($permalink_structure, '%category%') !== false) {
-		$cats = get_the_category($post->ID);
+if ( ! function_exists( 'c2c_single_category_postlink' ) ) :
+/**
+ * Returns post URI for a given post.
+ *
+ * If the post permalink structure includes %category%, then this function
+ * kicks into gear to reduce a hierarchical category structure to its lowest
+ * category.
+ *
+ * @param string $permalink The default URI for the post
+ * @param WP_Post $post The post
+ * @return string The post URI
+ */
+function c2c_single_category_postlink( $permalink, $post ) {
+	$permalink_structure = get_option( 'permalink_structure' );
+
+	if ( strpos( $permalink_structure, '%category%' ) !== false ) {
+		$cats = get_the_category( $post->ID );
 		if ( $cats )
-			usort($cats, '_usort_terms_by_ID'); // order by ID
+			usort( $cats, '_usort_terms_by_ID' ); // order by ID
 		$category = $cats[0]->slug;
-		if ( $parent=$cats[0]->parent )
-			$category = get_category_parents($parent, FALSE, '/', TRUE) . $category;
+
+		if ( $parent = $cats[0]->parent )
+			$category = get_category_parents( $parent, false, '/', true ) . $category;
+
 		// Now we know what the permalink component involving category hierarchy consists of.  Get rid of it.
-		$permalink = str_replace($category, $cats[0]->slug, $permalink);
-    }
+		$permalink = str_replace( $category, $cats[0]->slug, $permalink );
+	}
+
 	return $permalink;
 }
+endif;
 
-function single_category_catlink($catlink, $category_id) {
+if ( ! function_exists( 'c2c_single_category_catlink' ) ) :
+/**
+ * Returns category URI for a given category.
+ *
+ * If the given category is hierarchical, then this function kicks into gear to
+ * reduce a hierarchical category structure to its lowest category in the link.
+ *
+ * @param string $catlink The default URI for the category
+ * @param int $category_id The category ID
+ * @return string The category URI
+ */
+function c2c_single_category_catlink( $catlink, $category_id ) {
 	global $wp_rewrite;
 	$catlink = $wp_rewrite->get_category_permastruct();
 
-	if ( empty($catlink) ) {
-		$file = get_option('home') . '/';
+	if ( empty( $catlink ) ) {
+		$file = get_option( 'siteurl' ) . '/';
 		$catlink = $file . '?cat=' . $category_id;
 	} else {
-		$category = &get_category($category_id);
+		$category = &get_category( $category_id );
 		if ( is_wp_error( $category ) )
 			return $category;
 		$category_nicename = $category->slug;
 
 		//$catlink = str_replace('/category/', '/', $catlink);
-		$catlink = str_replace('%category%', $category_nicename, $catlink);
-		$catlink = get_option('home') . user_trailingslashit($catlink, 'category');
+		$catlink = str_replace( '%category%', $category_nicename, $catlink );
+		$catlink = get_option( 'siteurl' ) . '/' . user_trailingslashit( $catlink, 'category' );
 	}
+
 	return $catlink;
 }
+endif;
 
-add_filter('post_link', 'single_category_postlink', 10, 2);
-add_filter('category_link', 'single_category_catlink', 10, 2);
+if ( ! function_exists( 'c2c_single_category_redirect' ) ) :
+/**
+ * Redirects fully hierarchical category links to the single category link.
+ *
+ * @since 2.0
+ *
+ * @uses filter c2c_single_category_redirect_code
+ */
+function c2c_single_category_redirect() {
+	global $wp_query, $post;
+	$redirect = null;
+	$category_name = isset( $wp_query->query['category_name'] ) ? $wp_query->query['category_name'] : '';
+
+	if ( is_category() ) {
+		if ( ! empty( $category_name ) && $category_name != $wp_query->query_vars['category_name'] )
+			$redirect = c2c_single_category_catlink( '', $wp_query->query_vars['cat'] );
+	}
+	elseif ( is_single() ) {
+		if ( ! empty( $category_name ) && substr_count( $category_name, '/' ) > 1 )
+			$redirect = get_permalink( $post );
+	}
+
+	if ( $redirect )
+		wp_redirect( $redirect, apply_filters( 'c2c_single_category_redirect_status', 302 ) );
+}
+endif;
+
+add_filter( 'category_link',     'c2c_single_category_catlink', 10, 2 );
+add_filter( 'post_link',         'c2c_single_category_postlink', 10, 2 );
+add_filter( 'template_redirect', 'c2c_single_category_redirect' );
 
 ?>
